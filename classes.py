1b2e8d25f6f65d586cfe2ad3daef11395f46a368
ofek.harpaz <oharpaz1@Gmail.com>
2023-04-24 15:14:44 -0700

    new file1

--- classes.py
+++ classes.py
@@ -1,8 +1,12 @@
+import paramiko
+
+
 class RouterConnection:
-    def __init__(self, target, username, password):
-        self.target = target
+    def __init__(self, hostname, username, password):
+        self.hostname = hostname
         self.username = username
         self.password = password
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.client.connect(hostname=self.target, username=self.username, password=self.password)
+        self.client.connect(hostname=self.hostname, username=self.username, password=self.password)
+
--- main.py
+++ main.py
@@ -3,4 +3,15 @@ from classes import *       # class aveliable: "RouterConnection",
 from functools import *     # function avelable: "execute_command", "close", 
 
 
+# Create a new router connection object
+router = RouterConnection('hostname', 'username', 'password')
+
+# Execute a command on the router
+output = router.execute_command('show interfaces')
+
+# Print the output
+print(output)
+
+# Close the router connection
+router.close()
 
