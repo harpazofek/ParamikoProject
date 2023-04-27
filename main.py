9edca77e64296b885f19ba52401ca868e8fbb195
eli <eli4146@gmail.com>
2023-04-27 21:41:43 +0300

    added loop for testing if VM is up adain

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 import paramiko
+from pass1 import *
+from time import sleep
 
-target = 'IP'
 port = 22
-username = open('pass.txt', 'username')
-password = 'password'
+cmd = ""
 
-cmd = "ls"
+
 
 with paramiko.SSHClient() as client:
     client.load_system_host_keys()
     client.connect(target, port, username, password)
-    print(cmd)
-    (stdin, stdout, stderr) = client.exec_command(cmd)
-    output = stdout.read()
-    print(str(output, 'utf8'))
+    while True:
+        cmd = input(f'enter a command for the remote vm : (exit when done)')
+        if cmd == "exit" : break
+        (stdin, stdout, stderr) = client.exec_command(cmd)
+        output = stdout.read()
+        print(str(output, 'utf8'))
+        shel1 = client.invoke_shell()
+    for i in range(3):
+        print(f'checking if the VM is up !')
+        try:
+            print(client.get_host_keys())
+            (stdin, stdout, stderr) = client.exec_command("pwd")
+            output = stdout.read()
+            print(str(output, 'utf8'))
+        except:
+            print(f'VM is not up yet')
+        sleep(10)    
+    client.close()  
+        
