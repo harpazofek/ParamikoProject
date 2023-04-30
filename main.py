5d4658ec33976a878fd6c9be7626dd943347c668
Ofek Harpaz <oharpaz1@gmail.com>
2023-04-30 03:03:32 +0300

    working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from secret_password import *
 from functions import *
 
 is_remote = True
-ssh = ssh_connection(hostname, port, username, password)
+ssh_client = ssh_connection(hostname, port, username, password)
 trying_time = 0
 
 
@@ -11,25 +11,26 @@ while is_remote:
     cmd = input(f"enter a command for the remote vm : (or 'q' to quit) ").lower()
     if cmd == 'q':                                  # Check if user wants to quit
         print("Programing Shut Down")
-        is_remote = False
+        break
     else:
-        (stdin, stdout, stderr) = ssh.exec_command(cmd)
+        (stdin, stdout, stderr) = ssh_client.exec_command(cmd)
         output = stdout.read().decode()
         print(f"{output}")
         while True:                                 # Wait for the connection to become active again
             trying_time += 1
             try:
-                cmd = "whoami"                      # Try executing a command on the remote server
-                stdin, stdout, stderr = ssh.exec_command(cmd)
+                cmd = "date"                      # Try executing a command on the remote server
+                stdin, stdout, stderr = ssh_client.exec_command(cmd)
                 output = stdout.read().decode()
-                print(f"{output}")
+                print(f"{output}SSH connection is active")
                 break                               # Exit the loop when the connection is re-active
             except paramiko.SSHException as e:
-                print(f"Error executing command: {e}\nWaiting for VM to come back online... ")
-                time.sleep(1)
-            if trying_time >= 6:
-                print(f"\n{trying_time} attempts made. Recursive loop.")
-                is_remote = False
-        print("Done")
-    ssh.close()  
+                ssh_client.connect(hostname, port, username, password)
+                print(f"Error executing command: {e}\nSSH connection is not active... ")
+                time.sleep(5)
+                if trying_time >= 10:
+                    print(f"\n{trying_time} attempts made. Recursive loop.")
+                    break
+        print("Command Done")
+    ssh_client.close()  
 print(f"Client {hostname} connection close")
